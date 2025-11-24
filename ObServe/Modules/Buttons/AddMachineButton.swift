842125d1ac46ca46cbb8a83f20016e8b73f355d6
daniel <daniel.schatz6437@gmail.com>
2025-11-24 15:19:25 +0100

    feat: refactor AddMachineButton

--- ObServe/Modules/Buttons/AddMachineButton.swift
+++ ObServe/Modules/Buttons/AddMachineButton.swift
@@ -11,31 +11,32 @@ struct AddMachineButton: View {
     var action: () -> Void
 
     var body: some View {
-        ZStack {
             Button(action: action) {
-                Text("ADD MACHINE")
-                    .foregroundColor(Color.gray.opacity(0.7))
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 40)
-                    .background(
-                        LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.7), Color.black]), startPoint: .top, endPoint: .bottom)
-                    )
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 0)
-                            .stroke(Color.gray.opacity(0.4), lineWidth: 1)
-                    )
+                ZStack {
+                    
+                    Text("ADD MACHINE")
+                        .foregroundColor(Color.gray.opacity(0.7))
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 40)
+                        .background(
+                            LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.7), Color.black]), startPoint: .top, endPoint: .bottom)
+                        )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 0)
+                                .stroke(Color.gray.opacity(0.4), lineWidth: 1)
+                        )
+                }
+                .innerShadow(
+                    color: Color.gray.opacity(0.7),
+                    blur: 25,
+                    spread: 12,
+                    offsetX: 0,
+                    offsetY: 0,
+                    opacity: 0.1
+                )
             }
             .frame(maxWidth: 200)
             .padding(.horizontal, 5)
             .padding(.bottom, 32)
-        }
-        .innerShadow(
-            color: Color.gray.opacity(0.7),
-            blur: 25,
-            spread: 12,
-            offsetX: 0,
-            offsetY: 0,
-            opacity: 0.1
-        )
     }
 }
